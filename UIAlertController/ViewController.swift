e89e55a35dce533f328b77cc6c343caf452e1fbb
Anton Mishkevich <mian19@protonmail.com>
2021-12-02 23:05:48 +0300

    feat: add first Button

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func tapButton() {
+        let alertController = UIAlertController(title: "HEAD", message: "Hi, man", preferredStyle: .alert)
+        let button = UIAlertAction(title: "tap me, bitch", style: .default, handler: nil)
+        alertController.addAction(button)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 
 }
 
